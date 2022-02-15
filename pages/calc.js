52e46b8af9252efeb727ff983ed8ec8b5f88aff6
Your Name <nlimpag07@gmail.com>
2022-02-15 14:31:48 +0800

    fixes some issues

---
+++ pages/calc.js
@@ -0,0 +1,68 @@
+coursesOutlines: [
+  {
+    id: 1,
+    courseId: 2,
+    course: {
+      id: 2,
+      title: "Sample Course",
+      courseOutlines: [],
+      learners: [],
+    },
+    courseTitle: "Sample Course",
+    learnersCourseOutlines: [
+      {
+        id: 5,
+        courseOutlineId: 1,
+        course: {
+          id: 2,
+          title: "Sample Course",
+          courseOutlines: [],
+          learners: [],
+        },
+      },
+      {
+        id: 6,
+        courseOutlineId: 2,
+        course: {
+          id: 2,
+          title: "Sample Course",
+          courseOutlines: [],
+          learners: [],
+        },
+      },
+    ],
+  },
+  {
+    id: 2,
+    courseId: 2,
+    course: {
+      id: 2,
+      title: "Sample Course",
+      courseOutlines: [],
+      learners: [],
+    },
+    courseTitle: "Sample Course",
+    learnersCourseOutlines: [
+      {
+        id: 5,
+        courseOutlineId: 1,
+        course: {
+          id: 2,
+          title: "Sample Course",
+          courseOutlines: [],
+          learners: [],
+        },
+      },
+      {
+        id: 6,
+        courseOutlineId: 2,
+        course: {
+          id: 2,
+          title: "Sample Course",
+          courseOutlines: [],
+          learners: [],
+        },
+      },
+    ],
+  },
+];
