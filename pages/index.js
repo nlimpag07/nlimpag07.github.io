94e10031056e16fe932928ada0b96ebd4d950985
Your Name <nlimpag07@gmail.com>
2022-02-15 15:28:26 +0800

    Added maintenance page

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,22 @@
 import Navigation from "../components/Navigation";
-
-const Home = () => (
-  <article>
-    <Navigation />
-
-    <header>
-      <h1>Welcome to my Favorite Books</h1>
-    </header>
-
-    <section>
-      <header>
-        <h2>
-          Explore a few excerpts from my favorite books and I think everything
-          is good now
-        </h2>
-      </header>
-      <p>
-        Read the first few lines of the books that shaped my childhood fantasies
-        and inspired my adulthood.
-      </p>
-      <p>
-        This site created b: Noel Limpag <i>for learning purposes only...</i>
-      </p>
-    </section>
-  </article>
-);
+import Image from "next/image";
+const Home = () => {
+  const myLoader = ({ src, width, quality }) => {
+    return `/under_maintenance.png?w=${width}&q=${quality || 75}`;
+  };
+  return (
+    <article>
+      <section>
+        <Image
+          loader={myLoader}
+          src="/under_maintenance.png"
+          alt="Picture of the author"
+          width={666}
+          height={492}
+        />
+      </section>
+    </article>
+  );
+};
 
 export default Home;
